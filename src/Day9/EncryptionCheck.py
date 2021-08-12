f896cab56b55fd1c54ecdd3847c116222e1f288e
Jack-Development <shiltonjack@gmail.com>
2021-08-12 17:29:43 +0100

    Work started - Day 9

---
+++ src/Day9/EncryptionCheck.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+p = Path('.').absolute()
+resources = p.parents[1].joinpath('Resources')
+file = open(resources.joinpath('Day9.txt'), "r")
+cleanList = []
+for l in file:
+    cleanList.append(l.replace('\n', ''))
+cleanList.pop(0)
+
+for i in range(25, 26):
+    dataset = []
+    for j in range(0,25):
+        dataset.append(cleanList[i - j])
+    print(dataset)
+    print(cleanList[i])
